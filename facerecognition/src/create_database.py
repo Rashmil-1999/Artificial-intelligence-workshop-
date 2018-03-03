d39af41473499d8391ca5073a5c8b85d4859a69f
Rashmil Panchani <32737711+Rashmil-1999@users.noreply.github.com>
2018-03-03 22:42:32 +0530

    Update create_database.py

--- facerecognition/src/create_database.py
+++ facerecognition/src/create_database.py
@@ -15,15 +15,18 @@ if not os.path.isdir(path):
     os.mkdir(path)
 face_cascade = cv2.CascadeClassifier("../trained_models/haarcascade_frontalface_default.xml")
 cap=cv2.VideoCapture(0)
-while count<100:
+while count<100: #you can change the number of times the loop is executed ,set it to the number images you want to capture  
     ret_val, frame = cap.read()
     if ret_val == True:
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.1, 5)
         if faces.any() :
             face = max(faces, key = lambda x : x[2]*x[3])
-            #faces = sorted(faces, key = lambda x : x[2]*x[3])
-            #face = faces[-1]
+            '''
+            i have defined two types code snippet to find maximum area of the face to be detected.
+            faces = sorted(faces, key = lambda x : x[2]*x[3])
+            face = faces[-1]
+            '''
             roi = gray[face[1]:face[1]+face[3],face[0]:face[0]+face[2]]
             roi = cv2.resize(roi, (100,100))
             cv2.imwrite(path+'/'+str(count)+'.jpg', roi)
